747648b4af6d4b9af93a3ff7dc5ce1b0cb797fd8
Aaron J. Miles <aaron.miles@rmays.com>
2021-09-21 12:23:53 +0100

    Wrapped all components in App.js with GlobalState.

--- src/App.js
+++ src/App.js
@@ -4,12 +4,15 @@ import { Balance } from './components/Balance';
 import { IncomeExpense } from './components/IncomeExpense';
 import { TransactionList } from './components/TransactionList';
 import { AddTransaction } from './components/AddTransaction';
+
+import { GlobalProvider } from './context/GlobalState';
+
 import './App.css';
 
 
 function App() {
   return (
-    <div>
+    <GlobalProvider>
       <Header />
       <div className="container">
           <Balance />
@@ -17,7 +20,7 @@ function App() {
           <TransactionList />
           <AddTransaction />
       </div> 
-    </div>
+    </GlobalProvider>
   );
 }
 
